8ad2d6e2c1765ac2d1834282d4e1cda93937873f
hamadz1992 <34457332+hamadz1992@users.noreply.github.com>
2026-08-03 23:55:34 +0100

    Initial project setup

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+function App() {
+  return (
+    <main className="min-h-screen flex items-center justify-center bg-white">
+      <div className="text-center">
+        <h1 className="text-5xl font-bold text-gray-900">
+          كل شيء بالمعقول
+        </h1>
+
+        <p className="mt-4 text-gray-600">
+          الموقع الرسمي لمحل Belma3qoul
+        </p>
+      </div>
+    </main>
+  );
+}
+
+export default App;
